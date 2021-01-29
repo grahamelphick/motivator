b885e3786ab142a708ead94ee76dac3cca0e8850
Gragit <gme2468@gmail.com>
2021-01-29 16:42:43 +0800

    added basic navbar and mantra components

---
+++ client/src/components/Mantra/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Mantra() {
+    return (
+        <div>
+            <input placeholder="Add your mantra here:"></input>
+        </div>
+    );
+}
+
+export default Mantra;
