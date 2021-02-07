6feea9de84264fcefdc0a8c6dc01de87c2d145ce
Gragit <gme2468@gmail.com>
2021-02-07 19:54:30 +0800

    goals show on Dashboard

--- client/src/components/GoalsList/index.js
+++ client/src/components/GoalsList/index.js
@@ -13,3 +13,48 @@
 // export default function GoalListItem({ children }) {
 //   return <li className="list-group-item">{children}</li>;
 // }
+
+
+import React, { useState, useEffect } from "react";
+import API from "../../utils/API";
+import Col from "../Col";
+import { List, ListItem } from "../List";
+
+
+
+function GoalsList() {
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        loadBooks()
+    }, [])
+
+    // Loads all books and sets them to books
+    function loadBooks() {
+        API.getBooks()
+            .then(res =>
+                setBooks(res.data)
+            )
+            .catch(err => console.log(err));
+    };
+    return (
+        <div>
+            <Col size="md-6 sm-12">
+                <h1>Books On My List</h1>
+                <List>
+                    {books.map(book => (
+                        <ListItem key={book._id}>
+                            {book.goal}
+                            {/* <DeleteBtn onClick={() => deleteBook(book._id)} /> */}
+                        </ListItem>
+                    ))}
+                </List>
+            </Col>
+        </div>
+    )
+}
+                    
+                
+                
+
+    export default GoalsList;
--- client/src/pages/Goals.js
+++ client/src/pages/Goals.js
@@ -1,32 +1,111 @@
-import React from "react";
-import { Route } from 'react-router-dom'
-import GoalsList from "../components/GoalsList";
+import React, { useState, useEffect } from "react";
+// import Mantra from "../components/Mantra";
+import { List, ListItem } from "../components/List";
+// import ResourcesGoals from "../components/ResourcesGoals";
+// import Journal from "../components/Journal";
 import Container from "../components/Container";
 import Col from "../components/Col";
 import Row from "../components/Row";
 import "./pages.css";
-import Button from "../components/Button";
-import Resources from "../components/ResourcesList";
+import API from "../utils/API";
+import { Input, TextArea, FormBtn } from "../components/Form";
+// import { Link } from "react-router-dom";
 
-function GoalPage() {
+function Goals() {
+    // Setting our component's initial state
+    const [books, setBooks] = useState([])
+    const [formObject, setFormObject] = useState({})
+
+    // Load all books and store them with setBooks
+    useEffect(() => {
+        loadBooks()
+    }, [])
+
+    // Loads all books and sets them to books
+    function loadBooks() {
+        API.getBooks()
+            .then(res =>
+                setBooks(res.data)
+            )
+            .catch(err => console.log(err));
+    };
+
+    // Deletes a book from the database with a given id, then reloads books from the db
+    // function deleteBook(id) {
+    //     API.deleteBook(id)
+    //         .then(res => loadBooks())
+    //         .catch(err => console.log(err));
+    // }
+
+    // Handles updating component state when the user types into the input field
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormObject({ ...formObject, [name]: value })
+
+    };
+
+    // When the form is submitted, use the API.saveBook method to save the book data
+    // Then reload books from the database
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        if (formObject.goal) {
+            API.saveBook({
+                goal: formObject.goal,
+                completed: formObject.completed
+            })
+                .then(res => loadBooks())
+                .catch(err => console.log(err));
+        }
+    };
 
     return (
-        <Container>
+        <Container fluid>
             <Row>
                 <Col size="md-6">
-                    <GoalsList></GoalsList>
+                    <h1>What Books Should I Read?</h1>
+                    <form>
+                        <Input
+                            onChange={handleInputChange}
+                            name="goal"
+                            placeholder="Title (required)"
+                        />
+                        {/* <Input
+                            onChange={handleInputChange}
+                            name="author"
+                            placeholder="Author (required)"
+                        />
+                        <TextArea
+                            onChange={handleInputChange}
+                            name="synopsis"
+                            placeholder="Synopsis (Optional)"
+                        /> */}
+                        <FormBtn
+                            disabled={!(formObject.goal)}
+                            onClick={handleFormSubmit}
+                        >
+                            Submit Book
+              </FormBtn>
+                    </form>
                 </Col>
-                <Col size="md-6">
-                    <Route render={({ history }) => (
-                        <Button
-                            onClick={() => { history.push('/new-goal') }}
-                        >Start something new:</Button>
-                    )} />
-                    <Resources></Resources>
+                <Col size="md-6 sm-12">
+                    <h1>Books On My List</h1>
+                    {books.length ? (
+                        <List>
+                            {books.map(book => (
+                                <ListItem key={book._id}>
+                                    {book.goal}
+                                    {/* <DeleteBtn onClick={() => deleteBook(book._id)} /> */}
+                                </ListItem>
+                            ))}
+                        </List>
+                    ) : (
+                            <h3>No Results to Display</h3>
+                        )}
                 </Col>
             </Row>
         </Container>
     );
 }
 
-export default GoalPage;
+
+export default Goals;
